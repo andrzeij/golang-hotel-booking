f231ae9c70aefcedac721c432be75d7e2cb62ce3
Andrew Limaru <andrew.limaru@roarsoftware.com.au>
2022-05-18 19:01:39 +1000

    Switch router from pat to go-chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,14 +5,16 @@ import (
 
 	"github.com/andreijy/golang-hotel-booking/pkg/config"
 	"github.com/andreijy/golang-hotel-booking/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter();
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
