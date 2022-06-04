27f58318848d3922e1e2594e2487d439139b0194
Andrew Limaru <andrew.limaru@gmail.com>
2022-06-04 15:53:24 +1000

    setup static homepage

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,5 +18,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	
+	fileserver := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
+
 	return mux
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,4 @@ require github.com/go-chi/chi/v5 v5.0.7
 
 require github.com/justinas/nosurf v1.1.1
 
-require github.com/alexedwards/scs/v2 v2.5.0 // indirect
+require github.com/alexedwards/scs/v2 v2.5.0
